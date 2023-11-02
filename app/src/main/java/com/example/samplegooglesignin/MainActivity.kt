856473047b44f09df22e4e30b39a115025085f12
Naoki Nishizawa <mino24zawawork@gmail.com>
2023-11-02 22:29:52 +0900

    create needs button

--- app/src/main/java/com/example/samplegooglesignin/MainActivity.kt
+++ app/src/main/java/com/example/samplegooglesignin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.samplegooglesignin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -10,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,11 +37,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(
             onClick = { /* Do something */ },
         ) {
             Text("Google Sign In Pattern 1")
+        }
+
+        Button(
+            onClick = { /* Do something */ },
+        ) {
+            Text("Google Sign In Pattern 2")
+        }
+        Button(
+            onClick = { /* Do something */ },
+        ) {
+            Text("Google Sign Out")
         }
     }
 }
